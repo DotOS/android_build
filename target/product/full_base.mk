0e505ce0916f8a48339aa7ff43fa5d5616c3695c
Abhisek Devkota <ciwrl@cyanogenmod.com>
2012-03-03 22:53:06 +0100

    Remove extra product property overrides
    This appears to clash with vendor common.mk for overrides;
    
    Change-Id: I5ec990d3c5ad56b573c45f1a3b20169c989851a0

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -37,11 +37,6 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     PhotoTable
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Zen.ogg \
-    ro.config.notification_sound=Chime.ogg
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
