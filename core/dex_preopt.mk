8cb56d7cee5e988bbc764255d00755d15d56d781
Alex Naidis <alex.naidis@linux.com>
2016-09-11 13:12:45 +0200

    core: dex_preopt: don't include additional debug information on user builds
    * This causes the size of boot.oat to be increased by 3.5%
    
    [RJ Trujillo <certifiedblyndguy@gmail.com>]: Only exempt user builds from
    debugging just in case something critical needs to be assessed
    
    Change-Id: I715cd6b4559b62a380a9265aeda5b725679409b9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -59,9 +59,11 @@ ifeq ($(HOST_OS),linux)
     # and still allows a simple workflow: building in frameworks/base and syncing.
     WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
   endif
-  # Add mini-debug-info to the boot classpath unless explicitly asked not to.
-  ifneq (false,$(WITH_DEXPREOPT_DEBUG_INFO))
-    PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
+  # Add mini-debug-info to the boot classpath on non-user builds only.
+  ifneq (user,$(TARGET_BUILD_VARIANT))
+    ifneq (false,$(WITH_DEXPREOPT_DEBUG_INFO))
+      PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
+    endif
   endif
 
   # Non eng linux builds must have preopt enabled so that system server doesn't run as interpreter
