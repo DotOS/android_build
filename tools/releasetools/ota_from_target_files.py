b52590d069331a0863bf534f1f04a39cc532cbf4
Mohan CM <mohanmanjappa@gmail.com>
2017-11-12 00:46:22 +0530

    make magisk as optional for root
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -804,14 +804,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
-  if block_based:
-    script.Print(" ")
-    script.Print("Flashing Magisk...")
-    script.Print(" ")
-    common.ZipWriteStr(output_zip, "Magisk/Magisk.zip",
-                   ""+input_zip.read("SYSTEM/addon.d/Magisk.zip"))
-    script.FlashMagisk()
-    script.Print(" ")
+  if os.getenv('ROOT_METHOD','rootless') == "magisk":
+    if block_based:
+      script.Print(" ")
+      script.Print("Flashing Magisk...")
+      script.Print(" ")
+      common.ZipWriteStr(output_zip, "Magisk/Magisk.zip",
+                     ""+input_zip.read("SYSTEM/addon.d/Magisk.zip"))
+      script.FlashMagisk()
+      script.Print(" ")
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
