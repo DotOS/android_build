a3e4a35d0d958bd45327d4fed30051735d0be168
Alex Cruz <du.alexcruz@gmail.com>
2018-01-25 21:26:21 +0530

    Theme DUI dialogs & things [2/2]
    Change-Id: Ic02c05c9c08e027b8bfff415c5d49ee643ffbbda

--- target/product/core.mk
+++ target/product/core.mk
@@ -50,6 +50,8 @@ PRODUCT_PACKAGES += \
     ProxyHandler \
     QuickSearchBox \
     Settings \
+    DuiDarkTheme \
+    DuiBlackTheme \
     SharedStorageBackup \
     StorageManager \
     Telecom \
