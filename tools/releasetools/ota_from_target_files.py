8534e29540fe64a278d9feb4fe656124bcb16c0a
maxwen <max.weninger@gmail.com>
2018-08-25 12:20:02 -0500

    Add clean cache script [3/3]
    Change-Id: Id3dd04b39296725190db6d05a1ba9fb2e9953111

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -847,6 +847,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.RunBackup("backup")
     script.Unmount("/system")
 
+  script.Mount("/system")
+  script.RunCleanCache()
+  script.Unmount("/system")
+
   system_progress = 0.75
 
   if target_info.GetBuildProp("ro.dot.display.version") is not None:
