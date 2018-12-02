67a2d207746c2eaabe0084955b7e7e87dd9ca24f
Mohan Cm <mohanmanjappa@gmail.com>
2018-12-02 12:16:14 +0530

    include custom sepolicy from now
    Change-Id: I093ed30c1fa3492c9595b2509f7d247103dfa91d

--- core/config.mk
+++ core/config.mk
@@ -1098,7 +1098,8 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include device/aosp/sepolicy/common/sepolicy.mk)
+## dotOS Custom Sepolicy
+$(eval include device/dot/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
