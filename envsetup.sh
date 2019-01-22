6f75b756ef4a7fbc886f53a8fa43043e8cd7138b
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-22 15:50:52 +0100

    Don't export BUILD_NUMBER in envsetup
    * The value is meant to be set only before running an /official/ build
    * This allows us to execute consecutive make commands without
      running kati for every invocation
    
    Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e

--- envsetup.sh
+++ envsetup.sh
@@ -141,7 +141,6 @@ function check_product()
     fi
     if (echo -n $1 | grep -q -e "^dot_") ; then
         CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^dot_//g')
-        export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         CUSTOM_BUILD=
     fi
